dbc722e9c1179af58059138bf959d12411a897c6
profawxhawk <35334693+profawxhawk@users.noreply.github.com>
2018-10-26 22:27:53 +0530

    Added src folder

---
+++ src/mainok.java
@@ -0,0 +1,14 @@
+import javafx.stage.Stage;
+import javafx.application.*;
+
+public class mainok extends Application
+{
+    public	static	void main(String[]	args)
+    {
+        System.out.println("Hello");
+	    launch(args);
+    }
+    @Override
+        public	void	start(Stage	primaryStage)	{
+    }
+}
