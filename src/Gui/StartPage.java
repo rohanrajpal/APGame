e383887c89335b33099d85c542b001186ef6a996
rohanrajpal <rohan17089@iiitd.ac.in>
2018-10-27 22:42:30 +0530

    Made the main menu UI

---
+++ src/Gui/StartPage.java
@@ -0,0 +1,43 @@
+package Gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartPage extends Application {
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Main Menu");
+
+        initmainlayout();
+    }
+
+    private void initmainlayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(StartPage.class.getResource("../view/StartPageLayout.fxml"));
+            rootLayout =  loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/SnakevsBloc.java
+++ src/SnakevsBloc.java
@@ -5,7 +5,6 @@ public class SnakevsBloc extends Application
 {
     public	static	void main(String[]	args)
     {
-        System.out.println("Hello");
 	    launch(args);
     }
     @Override
