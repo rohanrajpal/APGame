86440563b3d038be138b6f77252c376847ceb519
rohanrajpal <rohan17089@iiitd.ac.in>
2018-10-27 22:54:46 +0530

    Made the main menu UI

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button stGame;
+
+    @FXML
+    private Button btLeaderBoard;
+
+    @FXML
+    private Button btResume;
+
+    public void Message(ActionEvent actionEvent) {
+        System.out.println("Whaat");
+    }
 }
