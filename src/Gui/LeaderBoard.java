22f90cfc5ae6388ce489cd7c22ca6a15626ef41b
profawxhawk <35334693+profawxhawk@users.noreply.github.com>
2018-10-28 17:23:19 +0530

    corrected bugs : leaderboard is completed

--- src/Gui/LeaderBoard.java
+++ src/Gui/LeaderBoard.java
@@ -1,12 +1,12 @@
 package Gui;
 
+import controller.ControllerLeaderboard;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class LeaderBoard {
@@ -16,6 +16,7 @@ public class LeaderBoard {
 
     public LeaderBoard(){
         initmainlayout();
+
     }
     private void initmainlayout() {
         try {
@@ -23,7 +24,8 @@ public class LeaderBoard {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(StartPage.class.getResource("../view/LeaderBoardLayout.fxml"));
             rootLayout =  loader.load();
-
+            ControllerLeaderboard controller = loader.getController();
+            controller.leaderboard();
             // Show the scene containing the root layout.
 
         } catch (IOException e) {
@@ -31,6 +33,7 @@ public class LeaderBoard {
         }
     }
     public void show(Stage primaryStage){
+
         Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
         primaryStage.show();
--- src/controller/ControllerLeaderboard.java
+++ src/controller/ControllerLeaderboard.java
@@ -1,29 +1,38 @@
 package controller;
 
 import Gui.LeaderBoard;
+import Gui.StartPage;
 import Model.LeaderBoardModel;
 import Model.LeaderBoardelements;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.text.*;
 import java.io.IOException;
 import java.util.Date;
+import javafx.fxml.Initializable;;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerLeaderboard {
+
+public class ControllerLeaderboard implements Initializable {
     @FXML
-    private static GridPane Grid1;
+    private GridPane Grid1;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+    public void back(ActionEvent actionEvent){
+        StartPage gui=new StartPage();
+        gui.start((Stage) ((Node) actionEvent.getSource()).getScene().getWindow());
 
-
-    public static void leaderboard(ActionEvent event) throws IOException {
+    }
+    public  void leaderboard() throws IOException {
         LeaderBoardModel L=new LeaderBoardModel();
-       Grid1=new GridPane();
+
 //        try{
 //            LeaderBoardModel temp=LeaderBoardModel.deserialize();
 //            L.setLeaders(temp.getLeaders());
@@ -32,16 +41,24 @@ public class ControllerLeaderboard {
 //            LeaderBoardModel.serialize(L);
 //        }
         L.getLeaders().add(new LeaderBoardelements(1,100,new Date()));
+        L.getLeaders().add(new LeaderBoardelements(2,200,new Date()));
         for(int i=0;i<L.getLeaders().size();i++){
-
-            Grid1.add(new Label(Integer.toString(L.getLeaders().get(i).getIndex())),0,i+1);
-            Grid1.add(new Label(Integer.toString(L.getLeaders().get(i).getscore())),1,i+1);
+            Label l=new Label();
+            l.setText(Integer.toString(L.getLeaders().get(i).getIndex()));
+            GridPane.setHalignment(l, HPos.CENTER);
+            Grid1.add(l,0,i+1);
+            Label l1=new Label();
+            l1.setText(Integer.toString(L.getLeaders().get(i).getscore()));
+            GridPane.setHalignment(l1, HPos.CENTER);
+            Grid1.add(l1,1,i+1);
+            Label l2=new Label();
             DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
             String strDate = dateFormat.format(L.getLeaders().get(i).getDateofcreation());
-            Grid1.add(new Label(strDate),2,i+1);
+            l2.setText(strDate);
+            GridPane.setHalignment(l2, HPos.CENTER);
+            Grid1.add(l2,2,i+1);
         }
-        LeaderBoard gui=new LeaderBoard();
-        gui.show((Stage) ((Node) event.getSource()).getScene().getWindow());
+
 
     }
 }
